1a5dac65b1c042959f0ae80642be7063814adda7
shruti kumari <08shrutichaudhary@gmail.com>
2026-03-17 22:10:09 +0530

    Added Account model with validation and transaction methods

---
+++ src/model/Account.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Account {
+
+    private int id;
+    private String name;
+    private double balance;
+
+    // Constructor
+    public Account(int id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // Getters (Encapsulation)
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Deposit
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid deposit amount");
+            return;
+        }
+        balance += amount;
+    }
+
+    // Withdraw
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount");
+            return;
+        }
+        if (amount > balance) {
+            System.out.println("Insufficient balance");
+            return;
+        }
+        balance -= amount;
+    }
+}
