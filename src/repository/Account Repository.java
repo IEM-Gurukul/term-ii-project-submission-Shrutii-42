9bbe7e73565d6ab90233c3769cd0946288a97d2a
shruti kumari <08shrutichaudhary@gmail.com>
2026-03-18 23:29:52 +0530

    Created AccountRepository with basic CRUD operations

---
+++ src/repository/Account Repository.java
@@ -0,0 +1,21 @@
+package repository;
+
+import model.Account;
+import java.util.*;
+
+public class AccountRepository {
+
+    private Map<Integer, Account> accounts = new HashMap<>();
+
+    public void addAccount(Account acc) {
+        accounts.put(acc.getId(), acc);
+    }
+
+    public Account getAccount(int id) {
+        return accounts.get(id);
+    }
+
+    public List<Account> getAllAccounts() {
+        return new ArrayList<>(accounts.values());
+    }
+}
